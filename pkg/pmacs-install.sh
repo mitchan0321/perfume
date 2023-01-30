9728366aadbb3224a248c3693c6e0dcf249c68f2
SATO Mitsuhide <sato.mitsuhide@gmail.com>
2023-01-30 14:14:41 +0900

    new file package install for root.

---
+++ pkg/pmacs-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CWD=`pwd`
+cd ./bin
+
+sed -e s%@PREFIX@%${CWD}%g < pmacs.in > pmacs
+chmod 755 pmacs
+sed -e s%@PREFIX@%${CWD}%g < pmacs-client.in > pmacs-client
+chmod 755 pmacs-client
+
+exit 0
