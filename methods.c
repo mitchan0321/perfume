d3fcc449e88d8713ce7fbead038832790a5c10ea
SATO Mitsuhide <sato.mitsuhide@gmail.com>
2025-12-23 16:24:37 +0900

    Add commnad String::truncate for truncate N size characters from string.

--- methods.c
+++ methods.c
@@ -3902,6 +3902,34 @@ error2:
 }
 
 Toy_Type*
+mth_string_truncate(Toy_Interp *interp, Toy_Type *posargs, Hash *nameargs, int arglen) {
+    Toy_Type *self, *b;
+    int is, sts;
+    Cell *ss;
+    
+    if (arglen != 1) goto error;
+    if (hash_get_length(nameargs) > 0) goto error;
+    self = SELF(interp);
+    if (GET_TAG(SELF(interp)) != STRING) goto error2;
+    b = list_get_item(posargs);
+    if (GET_TAG(b) != INTEGER) goto error;
+    is = mpz_get_si(b->u.biginteger);
+    ss = self->u.string;
+    sts = cell_truncate(ss, is);
+    if (sts == 0) {
+        return const_Nil;
+    } else {
+        return const_T;
+    }
+    
+error:
+    return new_exception(TE_SYNTAX, L"Syntax error at 'truncate!', syntax: String truncate!", interp);
+
+error2:
+    return new_exception(TE_TYPE, L"Type error.", interp);
+}
+
+Toy_Type*
 mth_block_eval(Toy_Interp *interp, Toy_Type *posargs, Hash *nameargs, int arglen) {
     Toy_Type *closure, *result;
 
@@ -6623,6 +6651,7 @@ toy_add_methods(Toy_Interp* interp) {
     toy_add_method(interp, L"String", L"udecode",	mth_string_udecode,     L"encoding");
     toy_add_method(interp, L"String", L"uencode",	mth_string_uencode,     L"encoding");
     toy_add_method(interp, L"String", L"int-base",	mth_string_intbase, 	L"base");
+    toy_add_method(interp, L"String", L"truncate!",	mth_string_truncate, 	L"size");
 
     toy_add_method(interp, L"File", L"init", 		mth_file_init, 		L"mode:,mode,file-path");
     toy_add_method(interp, L"File", L"open", 		mth_file_open, 		L"mode:,mode,file-path");
